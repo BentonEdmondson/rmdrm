daa0e8db00f89370f7679f794b113b8c44bfcb31
benton <bentonedmondson@gmail.com>
2021-07-17 12:22:44 -0500

    building and execution is successful

--- flake.nix
+++ flake.nix
@@ -4,18 +4,17 @@
   outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {
 
     defaultPackage.x86_64-linux =
-      pkgs.pythonPackages.buildPythonApplication {
+      pkgs.stdenv.mkDerivation {
         pname = "inept-epub";
         version = "7.0";
-
-        src = ./.;
-        phases = [ "installPhase" ];
-        buildInputs = [ /*pythonPackages.pyopenssl*/ ];
-
+        src = self;
+        buildInputs = [ pkgs.python3 ];
         installPhase = ''
             mkdir -p $out/bin
+            chmod +x inept-epub
             cp inept-epub $out/bin
         '';
-    };
+      };
+
   };
 }
