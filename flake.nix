627232a1ab76c8ab5fd97663f119f14c6e20d8ef
benton <bentonedmondson@gmail.com>
2021-07-18 18:00:56 -0500

    cleaned up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,19 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
-  outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {
-
-    defaultPackage.x86_64-linux =
-      pkgs.stdenv.mkDerivation {
+  outputs = { self, ... }@flakes: let
+    nixpkgs = flakes.nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    defaultPackage.x86_64-linux = nixpkgs.stdenv.mkDerivation {
         pname = "inept-epub";
         version = "7.0";
         src = self;
-        buildInputs = [ (pkgs.python3.withPackages(ps: [ ps.pycrypto ps.pyopenssl ])) ];
+        buildInputs = [ (nixpkgs.python3.withPackages(python3Packages: [ python3Packages.pycrypto python3Packages.pyopenssl ])) ];
         installPhase = ''
             mkdir -p $out/bin
             chmod +x inept-epub
             cp inept-epub $out/bin
         '';
       };
-
   };
 }
