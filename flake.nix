d6ba98a7159f61e7b77dc17c8c6bb62e193c8831
benton <bentonedmondson@gmail.com>
2021-08-10 12:32:23 -0500

    fixed flake.nix formatting

--- flake.nix
+++ flake.nix
@@ -5,27 +5,33 @@
     nixpkgs = flakes.nixpkgs.legacyPackages.x86_64-linux;
   in {
     defaultPackage.x86_64-linux = nixpkgs.stdenv.mkDerivation {
-        pname = "inept-epub";
-        version = "7.0";
-        src = self;
-        buildInputs = [ (nixpkgs.python3.withPackages(python3Packages: [ python3Packages.pycrypto python3Packages.pyopenssl ])) ];
-        installPhase = ''
-            mkdir -p $out/bin
-            chmod +x inept-epub
-            cp inept-epub $out/bin
-        '';
+      pname = "inept-epub";
+      version = "7.0";
+      src = self;
 
-        meta = {
-          description = "A Python script to decrypt Adobe-encrypted EPUB files";
-          homepage = "https://github.com/BentonEdmondson/inept-epub";
-          license = [ nixpkgs.lib.licenses.gpl3Only ];
-          maintainers = [{
-            name = "Benton Edmondson";
-            email = "bentonedmondson@gmail.com";
-          }];
-          # potentially others, but I'm only listed those tested
-          platforms = [ "x86_64-linux" ];
-        };
+      buildInputs = [
+        (nixpkgs.python3.withPackages(
+          python3Packages: [ python3Packages.pycrypto python3Packages.pyopenssl ]
+        ))
+      ];
+
+      installPhase = ''
+          mkdir -p $out/bin
+          chmod +x inept-epub
+          cp inept-epub $out/bin
+      '';
+
+      meta = {
+        description = "A Python script to decrypt Adobe-encrypted EPUB files";
+        homepage = "https://github.com/BentonEdmondson/inept-epub";
+        license = [ nixpkgs.lib.licenses.gpl3Only ];
+        maintainers = [{
+          name = "Benton Edmondson";
+          email = "bentonedmondson@gmail.com";
+        }];
+        # potentially others, but I'm only listed those tested
+        platforms = [ "x86_64-linux" ];
       };
+    };
   };
 }
